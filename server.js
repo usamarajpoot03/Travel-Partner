4a3327b33f611e2f0d997eff8109e551d55ca0ee
usamarajpoot03 <usamarajpoot03@yahoo.com>
2021-08-31 20:05:49 +0500

    more functionality

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 const express = require("express");
 const app = express();
 
+// view engine
 app.set("view engine", "pug");
 
+// static content
+app.use(express.static(__dirname + "/public"));
+
+// port settings
 app.set("port", process.env.PORT || 3000);
 
+// routes
 app.get("/", function (req, res) {
   res.render("home");
 });
+
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+];
+
 app.get("/about", function (req, res) {
-  res.render("about");
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomFortune });
 });
 
+// error handlers
 app.use(function (req, res) {
   res.status(404);
   res.render("404");
 });
 
-// custom 500 page
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500);
