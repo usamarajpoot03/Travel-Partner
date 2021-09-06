1066ed36a32c4a8630aa0cf3e1802e12f7533adb
usamarajpoot03 <usamarajpoot03@yahoo.com>
2021-09-06 22:02:29 +0500

    error handling using domain context

--- server.js
+++ server.js
@@ -9,7 +9,45 @@ const credentials = require("./credentials");
 // emailService.send('joecustomer@gmail.com', 'Hood River tours on sale today!',
 //  'Get \'em while they\'re hot!');
 
-console.log("xx", "here");
+// Using Domain Context for error handling
+app.use(function (req, res, next) {
+  // create a domain for this request
+  var domain = require("domain").create();
+  // handle errors on this domain
+  domain.on("error", function (err) {
+    console.error("DOMAIN ERROR CAUGHT\n", err.stack);
+    try {
+      // failsafe shutdown in 5 seconds
+      setTimeout(function () {
+        console.error("Failsafe shutdown.");
+        process.exit(1);
+      }, 5000);
+      // disconnect from the cluster
+      var worker = require("cluster").worker;
+      if (worker) worker.disconnect();
+      // stop taking new requests
+      server.close();
+      try {
+        // attempt to use Express error route
+        next(err);
+      } catch (err) {
+        // if Express error route failed, try
+        // plain Node response
+        console.error("Express error mechanism failed.\n", err.stack);
+        res.statusCode = 500;
+        res.setHeader("content-type", "text/plain");
+        res.end("Server error.");
+      }
+    } catch (err) {
+      console.error("Unable to send 500 response.\n", err.stack);
+    }
+  });
+  // add the request and response objects to the domain
+  domain.add(req);
+  domain.add(res);
+  // execute the rest of the request chain in the domain
+  domain.run(next);
+});
 // Logging setup
 switch (app.get("env")) {
   case "development":
