1eb3cd16e0a45a89f2577be6d0f51e5837097f34
usamarajpoot03 <usamarajpoot03@yahoo.com>
2021-09-07 23:35:03 +0500

    auto views setup

--- server.js
+++ server.js
@@ -331,6 +331,22 @@ app.use(function (err, req, res, next) {
   res.render("500");
 });
 
+// directly serving files
+var autoViews = {};
+app.use(function (req, res, next) {
+  var path = req.path.toLowerCase();
+  // check cache; if it's there, render the view
+  if (autoViews[path]) return res.render(autoViews[path]);
+  // if it's not in the cache, see if there's
+  // a .handlebars file that matches
+  if (fs.existsSync(__dirname + "/views" + path + ".handlebars")) {
+    autoViews[path] = path.replace(/^\//, "");
+    return res.render(autoViews[path]);
+  }
+  // no view found; pass on to 404 handler
+  next();
+});
+
 // error handlers
 app.use(function (req, res) {
   res.status(404);
